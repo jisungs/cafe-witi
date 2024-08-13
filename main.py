a1b20f85a04686c34e886955c0e3417826910f33
Jisung Smart <jisung.smart@gmail.com>
2024-08-14 07:20:54 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
+import csv
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '29F9D7SD84F5GE9RE261M312k'
+Bootstrap5(app)
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('카페이름', validators=[DataRequired()])
+    location = StringField("지도 위치 구글맵(URL)", validators=[DataRequired(), URL()])
+    open = StringField("여는 시간 e.g. 8AM", validators=[DataRequired()])
+    close = StringField("닫는 시간 e.g. 5:30PM", validators=[DataRequired()])
+    coffee_rating = SelectField("커피", choices=["☕️", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
+    wifi_rating = SelectField("와이파이 신호", choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
+    power_rating = SelectField("전원", choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/add', methods=["GET", "POST"])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        with open("cafe-data.csv", mode="a", encoding='utf-8') as csv_file:
+            csv_file.write(f"\n{form.cafe.data},"
+                           f"{form.location.data},"
+                           f"{form.open.data},"
+                           f"{form.close.data},"
+                           f"{form.coffee_rating.data},"
+                           f"{form.wifi_rating.data},"
+                           f"{form.power_rating.data}")
+        return redirect(url_for('cafes'))
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template("cafes.html", cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
